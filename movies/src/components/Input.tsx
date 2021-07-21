244f2e39145cfa722bf870958a87798f3f433c1c
Tomás Garcia <tomy.garcia@hotmail.com>
2021-07-21 18:40:55 -0300

    csv input and submit

--- movies/src/components/Input.tsx
+++ movies/src/components/Input.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 import { Button } from 'react-bootstrap'
+import CSVReader from 'react-csv-reader'
+import {url} from '../config'
 
 export default function Input() {
+
+    const [input, setInput] = useState<string[]>([])
+    const fileLoad = (data:any[],fileInfo:any)=> {
+        let movies:string[] = []
+        data.forEach(movie => {
+            movies.push(movie[0])
+        })
+        setInput(movies)
+    }
+
+    const handleSubmit = ()=> {
+        console.log(input)
+        let moviesPost = []
+        // input.forEach(movie => {
+
+        // })
+
+        axios.post(`${url}/movies`,{name:input[0]}).then(resp => console.log(resp))
+    }
     return (
         <div>
-            <input type='file'  />
-            <Button>Submit</Button>
+            <CSVReader onFileLoaded={fileLoad} />
+            <Button onClick={handleSubmit}>Submit</Button>
         </div>
     )
 }
