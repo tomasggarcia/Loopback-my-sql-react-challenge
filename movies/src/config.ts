94bf33b3f2d06e1fd5be9c8f2f653e9a7fba359c
Tomás Garcia <tomy.garcia@hotmail.com>
2021-07-26 09:41:52 -0300

    url deploy

--- movies/src/config.ts
+++ movies/src/config.ts
@@ -1,2 +1,2 @@
-export const url = 'http://localhost:3001'
-// export const url = 'http://52.67.216.171:3001'
+// export const url = 'http://localhost:3001'
+export const url = 'http://52.67.216.171:3001'
