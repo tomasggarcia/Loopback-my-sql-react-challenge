f9348d590449ff5ad54d210d9a3c9d18da0157f0
Tomás Garcia <tomy.garcia@hotmail.com>
2021-07-26 12:34:17 -0300

    url changed

--- movies/src/config.ts
+++ movies/src/config.ts
@@ -1,3 +1,3 @@
-// export const url = 'http://localhost:3001'
+export const url = 'http://localhost:3001'
 
-export const url = 'http://52.67.216.171:3001'
+// export const url = 'http://18.229.137.95:3001'
